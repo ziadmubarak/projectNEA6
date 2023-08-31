5a5b0ab50141a8e144413e32f2ad8616fe7f4c57
ziadmubarak <ziadmubarak2424@gmail.com>
2023-08-31 09:14:49 +0400

    Enabled Debug Mode

--- run.py
+++ run.py
@@ -29,5 +29,5 @@ def load_user(user_id):
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-        app.run(host='0.0.0.0', port=5000)
+        app.run(host='0.0.0.0', port=5000, debug=True)
 
